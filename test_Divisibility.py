24e0c809e488103b723b2a7664959051a33a5c72
hundredvisionsguy <winikkc@hsd.k12.or.us>
2018-04-13 16:22:41 -0700

    adding regex tests to test_Divisibility

--- test_Divisibility.py
+++ test_Divisibility.py
@@ -3,9 +3,18 @@
 
 import unittest
 import Divisibility
+import re
 
 class KnownValues(unittest.TestCase):
-    
+    # Set up
+    def setUp(self):
+        # get text from script for regex tests
+        self.textfile = open('Divisibility.py', 'r')
+        self.scripttext = self.textfile.read()
+    # Tear down
+    def tearDown(self):
+        # close the file
+        self.textfile.close()
     ################################################
     # test divisibility
     ################################################
@@ -28,7 +37,16 @@ class KnownValues(unittest.TestCase):
     def test_divisibility_forNotEvenlyDivisible_7_2(self):
         result = Divisibility.divisibility(7,2)
         self.assertEquals("doesn't divide evenly", result)
-
+    # Test regex search for a pattern in script
+    def test_try_cmd_present(self):
+        matches = re.findall("try:", self.scripttext)
+        match = matches[0]
+        self.assertEquals("try:", match)
+        
+    def test_except_ValueError_present(self):
+        matches = re.findall("except ValueError:", self.scripttext)
+        match = matches[0]
+        self.assertEquals("except ValueError:", match)
 # Run the tests
 if __name__ == '__main__':
     unittest.main()
